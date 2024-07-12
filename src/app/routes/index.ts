613554bb5a72ce9dc8e341c0e96b7efafc8d5934
sarwar-asik <sarwarasik@gmail.com>
2024-07-12 14:29:21 +0600

    server: #config test done

---
+++ src/app/routes/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import { UserRouter } from '../modules/USER/user.route';
+import { AuthRouter } from '../modules/AUTH/auth.route';
+import { ImageRoutes } from '../modules/Image/image.route';
+
+const router = express.Router();
+
+const modulesRoutes = [
+  {
+    path: '/auth',
+    route: AuthRouter,
+  },
+  {
+    path: '/users',
+    route: UserRouter,
+  },
+ 
+];
+
+modulesRoutes.forEach(route => router.use(route.path, route.route));
+
+export default router;
---
+++ src/server.ts
@@ -0,0 +1,72 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-console */
+import mongoose from 'mongoose';
+// import config from './config/index.js';
+import 'colors';
+// import { logger, errorLogger } from './shared/logger';
+import app from './app';
+import { Server } from 'http';
+// import config from './config';
+import { errorLogger, logger } from './shared/logger';
+import config from './config/index';
+mongoose.set('strictQuery', false);
+
+process.on('uncaughtException', error => {
+  config.env === 'production'
+    ? errorLogger.error(error)
+    : console.log('uncaugthException is detected ......', error);
+  process.exit(1);
+});
+
+let server: Server;
+
+// console.log(config.data_url, 'config file Data'.red.bold);
+
+async function connection() {
+  try {
+    await mongoose.connect(config.database_url as string, {
+      dbName: 'Antopolis-Server',
+    });
+    config.env === 'production'
+      ? logger.info(`Database connection successful`.green.underline.bold)
+      : console.log(`Database connection successful`.green.underline.bold);
+
+    app.listen(config.port, (): void => {
+      config.env === 'production'
+        ? logger.info(
+            `Server is listening on port ${config.port}`.red.underline.bold
+          )
+        : console.log(
+            `Server is listening on port ${config.port}`.red.underline.bold
+          );
+    });
+  } catch (error) {
+    config.env === 'production'
+      ? errorLogger.error(`Failed to connect database: ${error}`.red.bold)
+      : console.log(`Failed to connect database: ${error}`.red.bold);
+  }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        config.env === 'production'
+          ? errorLogger.error(error)
+          : console.log(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
+}
+connection();
+
+process.on('SIGTERM', () => {
+  // logger.info('SIGTERM is received ....');
+  console.log('SIGTERM is received ....');
+  if (server) {
+    server.close();
+  }
+});
+
+// console.log(config.port,"url".green.bold);
