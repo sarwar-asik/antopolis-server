6e9843bedef3de3504704796663c626dc310d2aa
sarwar-asik <sarwarasik@gmail.com>
2024-07-13 10:21:41 +0600

    build-issue: fixed extra log @rendar

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,22 +1,22 @@
 import express from 'express';
-import { UserRouter } from '../modules/USER/user.route';
+// import { UserRouter } from '../modules/USER/user.route';
 
 import { ImageRoutes } from '../modules/Image/image.route';
-import { AuthRouter } from '../modules/AUTH/auth.route';
+// import { AuthRouter } from '../modules/AUTH/auth.route';
 import { CategoryRoutes } from '../modules/category/category.route';
 import { AnimalRoutes } from '../modules/animal/animal.route';
 
 const router = express.Router();
 
 const modulesRoutes = [
-  {
-    path: '/auth',
-    route: AuthRouter,
-  },
-  {
-    path: '/users',
-    route: UserRouter,
-  },
+  // {
+  //   path: '/auth',
+  //   route: AuthRouter,
+  // },
+  // {
+  //   path: '/users',
+  //   route: UserRouter,
+  // },
   {
     path: '/image',
     route: ImageRoutes,
